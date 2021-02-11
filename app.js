5dfaf9d493f4c3cfe7683f86cf60b36bfab3fb90
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-02-10 21:08:25 -0500

    version 2 added canvas

--- app.js
+++ app.js
@@ -1,3 +1,96 @@
+// create canvas
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+// adjust canvas width and height on resize
+const resizeCanvas = () => {
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+};
+resizeCanvas();
+window.addEventListener('resize', () => {
+    resizeCanvas();
+    location.reload();
+});
+
+// create snowflakes array
+const snowflakes = [];
+const numberOfFlakes = 300;
+
+// snowflake blueprint
+class Snowflake {
+    constructor(x, y, size, velocity, image, opacity) {
+        this.x = x;
+        this.y = y;
+        this.dx = size;
+        this.dy = size;
+        this.velocity = velocity;
+        this.image = image;
+        this.opacity = opacity;
+    }
+    draw() {
+        ctx.globalAlpha = this.opacity;
+        ctx.drawImage(this.image, this.x, this.y, this.dx, this.dy);
+    }
+    update() {
+        this.draw();
+        this.y += this.velocity * .5;
+        if (this.y + this.dy > canvas.height) {
+            this.y = canvas.height - this.dy;
+            setTimeout( () => {
+                this.y = Math.random() * canvas.height - (canvas.height + this.dy);
+                this.dx = Math.random() * 10 + 10;
+                this.dy = this.dx;
+            }, 1000)
+        }
+    }
+}
+
+// clear function to clear canvas every loop
+const clear = () => {
+    ctx.fillStyle = 'rgb(0, 0, 0)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+};
+
+// create snowflake
+const createSnowflake = () => {
+    
+    // create new image snowflake
+    const image = new Image();
+    image.src = './image/snowflake-regular.svg'
+
+    // loop through the numberOfFlakes and randomize values
+    for (let i = 0; i < numberOfFlakes; i++) {
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height - canvas.height;
+        const size = Math.random() * 10 + 10;
+        const velocity = Math.random();
+        const opacity = Math.random();
+
+        // push new snowflake into array
+        snowflakes.push(new Snowflake(x, y, size, velocity, image, opacity));
+    }
+};
+
+// animation loop
+const animate = () => {
+
+    // recursion
+    requestAnimationFrame(animate);
+
+    // clear canvas
+    clear();
+
+    // render snowflakes every loop
+    snowflakes.forEach( (snowflake) => {
+        snowflake.update();
+    });
+}
+
+// call functions
+createSnowflake();
+animate();
+
 // initialize constant variables for second, minute, hour and day
 const second = 1000;
 const minute = second * 60;
@@ -35,20 +128,6 @@ const countDown = setInterval( () => {
     }
 
 }, 1000);
-
-// add click event to about button and toggle modal window
-document.getElementById('about')
-    .addEventListener('click', () => {
-        document.querySelector('.modal')
-            .classList.toggle('active');
-    });
-
-// add click event to X and close modal window
-document.getElementById('close')
-    .addEventListener('click', () => {
-        document.querySelector('.modal')
-            .classList.toggle('active');
-    });
 
 
 
